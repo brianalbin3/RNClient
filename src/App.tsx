f213e1420119683973928097c8149216d278e3d5
Brian Albin <brianalbin3@gmail.com>
2020-12-07 23:40:13 -0500

    Make navbar work on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import EventIcon from '@material-ui/icons/Event';
 import SettingsPhoneIcon from '@material-ui/icons/SettingsPhone';
 import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-  }),
-);
+import './App.css';
 
 function App() {
-  const classes = useStyles();
-
-  // TODO: Change icons-container to nav
-  // TODO: Two headers
+  
   return (
     <div>
       <div className="app-bar-container">
         <AppBar position="static">
           <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className="title">RoboNurse</Typography>
+            <Typography variant="h6" className="title"></Typography>
             <nav>
                 <div className="icon-container">
                   <LocalHospitalIcon className="icon" />
